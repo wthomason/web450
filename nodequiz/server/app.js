0541d8d31eaaf912c7059c37c89557f6ab221a4e
Tyson Thomason <tthomason@mailserviceslc.com>
2019-09-26 14:27:45 -0500

    database connection and routes for nodequize

--- nodequiz/server/app.js
+++ nodequiz/server/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const Employee = require('./db-models/employee');
 
 let app = express();
 
@@ -19,9 +20,44 @@ const serverPort = 3000;
 
 /************************* Mongoose connection strings go below this line  ***************/
 
+const connString = 'mongodb+srv://web450:admin@cluster0-aqjgi.mongodb.net/web450?retryWrites=true&w=majority';
+
+mongoose.connect(connString, {promiseLibrary:require('bluebird'), useNewUrlParser: true})
+        .then(() => console.debug('Connection to the MongoDB instance was successful!'))
+        .catch((err) => console.debug('MongoDB Error: ' + err.message));
+
 
 /************************* API routes go below this line ********************/
 
+app.post('/api/employees', function(req, res, next) {
+  const employee = {
+    employeeId: req.body.employeeId,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname
+  };
+
+  Employee.create(employee, function(err, employees) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      console.log(employees);
+      res.json(employees);
+    }
+  });
+});
+
+app.get('/api/employees/:id', function(req, res, next) {
+  Employee.findOne({'employeeId': req.params.id}, function(err, employee) {
+    if (err) {
+      console.log(err);
+      return next(err);
+    }  else {
+      console.log(employee);
+      res.json(employee);
+    }
+  })
+});
 
 /**
  * Creates an express server and listens on port 3000
